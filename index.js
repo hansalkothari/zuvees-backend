4e285d03715ce8d93b3d5cabaea9bf802cc10f1a
Hansal Kothari <hansalkothari@Hansals-MacBook-Air.local>
2025-05-31 09:42:52 +0530

    changed cors endpoint

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import riderRoutes from './routes/riderRoutes.js';
 const app = express();
 const PORT = 3000;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://zuvees-assgnment.netlify.app', credentials: true }));
 app.use(express.json());
 
 // MongoDB connection. 
